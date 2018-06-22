58c0aca4e1af0fe61f9eb650462ed6fac29308ce
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-06-22 16:03:30 -0300

    Cria função para adicionar palavra

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ TrieNo.prototype.getPalavra = function() {
   }
   return saida.join('');
 };
+
+Trie.prototype.insere = function(palavra) {
+  var no = this.root;
+  for(var i = 0; i < palavra.length; i++) {
+    if (!no.filho[palavra[i]]) {
+      no.filho[palavra[i]] = new TrieNo(palavra[i]);
+      no.filho[palavra[i]].pai = no;
+    }
+    no = no.filho[palavra[i]];
+    if (i == palavra.length-1) {
+      no.fim = true;
+    }
+  }
+  var auxiliar = palavra[0].toUpperCase() + palavra.slice(1);
+  if(auxiliar != palavra){
+    trie.insere(auxiliar);
+  }
+};
